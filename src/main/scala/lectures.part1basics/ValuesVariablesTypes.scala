d03d027dbc6bc955181da10658a65153d4475454
Angela Xu <angelxu1116@gmail.com>
2022-10-05 18:54:39 -0500

    Add files via upload

---
+++ src/main/scala/lectures.part1basics/ValuesVariablesTypes.scala
@@ -0,0 +1,26 @@
+package lectures.part1basics
+
+object ValuesVariablesTypes extends App{
+
+  val x = 42
+  println(x)
+
+
+  //vals are immutable
+  val abc = {
+    val aString: String = "hello"
+    val y = 1 + 2
+    println(y)
+
+    if (y > 4) "hello" else "2"
+    1116
+  }
+
+  println(abc)
+
+  def aFunction()
+
+
+
+
+}
